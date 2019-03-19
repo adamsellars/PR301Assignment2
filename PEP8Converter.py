c7964d78ed32c1800b0fe4858e7a53a3697a2bd4
LeroiChua <48010810+LeroiChua@users.noreply.github.com>
2019-03-19 22:24:57 +1300

    Add files via upload

--- PEP8Converter.py
+++ PEP8Converter.py
@@ -7,23 +7,46 @@ class PEP8Converter:
 
     @staticmethod
     def convert_method(plant_method: str) -> str:
-        if ":" in plant_method:
-            method_and_type = plant_method.split(":")
-            return_type = method_and_type[1].strip()
-            method = method_and_type[0][0].lower() + method_and_type[0][1:].rstrip()
-            method_name = "\n    def {}:\n        return {}\n".format(method, return_type)
-            return method_name
-        else:
-            plant_method = plant_method[0].lower() + plant_method[1:]
-            method_name = "\n    def {}:\n        return\n".format(plant_method)
-            return method_name
+        print("My method: ", plant_method)
+        if "String" in plant_method:
+            plant_method = plant_method.replace("String", "str")
+        total_words = len(plant_method)
+        my_method = ""
+        for i in range(total_words):
+            if "(" in plant_method[i]:
+                print("here1")
+                for j in range(i, total_words):
+                    if ")" in plant_method[j]:
+                            plant_method = list(plant_method)
+                            print("here3")
+                            print("small stuff: ", plant_method[j+1])
+                            plant_method[j+1] = " ->"
+                            my_method = "".join(plant_method)
+
+
+        pep8_method = "\n    def {}:\n        pass\n".format(my_method)
+        return pep8_method
+
+        # no_colon_method = ""
+
+        # if ":" in plant_method:
+        #     method_and_type = plant_method.split(":")
+        #     print(method_and_type)
+            # return_type = method_and_type[1].strip()
+            # method = method_and_type[0][0].lower() + method_and_type[0][1:].rstrip()
+            # method_name = "\n    def {}:\n        return {}\n".format(method, return_type)
+            # return method_name
+        # else:
+            # plant_method = plant_method[0].lower() + plant_method[1:]
+            # method_name = "\n    def {}:\n        return\n".format(plant_method)
+            # return method_name
 
     @staticmethod
     def convert_attribute(plant_attribute: str) -> str:
         attribute_and_type = plant_attribute.split(":")
         return_type = attribute_and_type[1].strip()
         attribute = attribute_and_type[0][0].lower() + attribute_and_type[0][1:].strip()
-        an_attribute = "    {} = {}\n".format(attribute, return_type)
+        an_attribute = "    self.{} = {}\n".format(attribute, return_type)
         return an_attribute
 
     # def convert_constructor():
@@ -32,14 +55,13 @@ class PEP8Converter:
     def create_class(plant_class_name: str) -> str:
         methods = ""
         attributes = ""
-        constructor = "\n    def __init__(self): \n        pass\n"
-
         class_name = PEP8Converter.convert_class(plant_class_name.class_name)
         for aMethod in plant_class_name.method:
             methods += PEP8Converter.convert_method(aMethod)
         for an_attribute in plant_class_name.attribute:
             attributes += PEP8Converter.convert_attribute(an_attribute)
-        return class_name + attributes + constructor + methods
+        return class_name + attributes + methods
 
-
+    # @staticmethod
+    # def create_relationship():
 
