3a2596467fe0e72a72fb1b088a107c42be796bf7
LeroiChua <48010810+LeroiChua@users.noreply.github.com>
2019-03-20 15:00:31 +1300

    Delete Controller.py

--- Controller.py
+++
@@ -1,8 +0,0 @@
-class Controller:
-
-    def __init__(self, model: str, view: Object) -> None:
-        self.my_class_finder = ClassFinder    
-        self.view = View    
-    
-    def start_menu(self) -> None:
-        pass
