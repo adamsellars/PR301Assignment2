4496a9e42774d43eb199d054726578c4621c44d9
LeroiChua <48010810+LeroiChua@users.noreply.github.com>
2019-03-22 13:24:00 +1300

    Add files via upload
    salvaged?

--- View.py
+++ View.py
@@ -6,16 +6,21 @@ class View:
                "Press 1 to load your text file\n"
                "Press 2 to write from plantuml text to python code\n"
                "Press 3 to run the command line interpreter\n"
-               "Press 5 to exit"
+               "Press 4 to write file to data base\n"
+               "Press 5 to print PEP8 class file to screen\n"
+               "Press 6 to exit"
                "\n")
 
     def file_not_loaded_warning(self):
         print("Must load a file first")
 
     def file_empty_warning(self):
-        print("File must not be empty")
+        print("File must be empty")
 
     def file_type_warning(self):
         print("File must be a .txt file type")
+
+    # def file_not_PEP8_warning(self):
+    #     print("File must be converted to PEP8 first")
 
 
