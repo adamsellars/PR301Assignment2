ca366ddc8d7faee2dc519edd36752451ba0365b8
LeroiChua <48010810+LeroiChua@users.noreply.github.com>
2019-03-24 19:11:23 +1300

    Add files via upload

--- View/View.py
+++ View/View.py
@@ -8,7 +8,10 @@ class View:
                "Press 3 to run the command line interpreter\n"
                "Press 4 to write file to database\n"
                "Press 5 to print  class names from database to screen\n"
-               "Press 6 to exit"
+               "Press 6 to load text file, convert data to PEP8 python format then convert file to pickle\n"
+               "\t\tformat in same directory\n"
+               "Press 7 to load data from pickle file\n"
+               "Press 8 to exit"
                "\n")
 
     def file_not_loaded_warning(self):
