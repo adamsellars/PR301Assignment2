c952b0e12a8f818aa97c5c28b473fc2c0fc33d95
adamsellars <48400082+adamsellars@users.noreply.github.com>
2019-03-24 14:14:24 +1300

    Add files via upload

---
+++ Tests/TestsInterpreter.py
@@ -0,0 +1,134 @@
+"""
+# ---------------------------------------------Created by Adam----------------------------------
+# Testing greeting and help messages work as expected
+>>> cmd = CommandLineInterpreter(Controller(ClassFinder(), View()))
+>>> cmd.do_greet("line")
+==================================================
+Welcome to the command line interpreter.
+Type help to view available commands
+==================================================
+<BLANKLINE>
+
+# Test 2
+>>> cmd.do_help("")
+<BLANKLINE>
+Documented commands (type help <topic>):
+========================================
+greet  help  load_file  print_file  quit  write_file
+<BLANKLINE>
+
+# Test 3
+>>> cmd.do_help("greet")
+==================================================
+greet command help
+==================================================
+Description: A Greeting message
+Syntax: greet
+Parameter: none
+Example: greet
+<BLANKLINE>
+
+# Test 4
+>>> cmd.do_help("help")
+List available commands with "help" or detailed help with "help cmd".
+
+# Test 5
+>>> cmd.do_help("load_file")
+==================================================
+load_file command help
+==================================================
+Description: Load a .txt file into the program
+Syntax: load_file [path]
+Parameter: [path] = full path name of the file starting from the root directory of this program
+Example: load_file test4(myowncode).txt
+<BLANKLINE>
+
+# Test 6
+>>> cmd.do_help("quit")
+==================================================
+quit command help
+==================================================
+Description: terminate the command line interpreter
+Syntax: quit
+Parameter: none
+Example: quit
+<BLANKLINE>
+
+# Test 7
+# surprise.txt doesn't exist should get an error message
+>>> cmd.do_load_file("surprise.txt")
+Error, file does not exist!
+
+# Test 8
+# this file does exist should get a success message
+>>> cmd.do_load_file("test4.txt")
+<BLANKLINE>
+file loaded...
+<BLANKLINE>
+
+# Test 9
+>>> cmd.do_quit("")
+Goodbye thank you for using the command line interpreter
+True
+
+# ---------------------------------------------Created by Leroi---------------------------------
+# Test 10
+>>> cmd.help_print_file()
+==================================================
+print_file command help
+==================================================
+Description: Print a PEP8 format text into the interpreter
+Syntax: print_file
+Example: print_file test4.txt
+<BLANKLINE>
+
+# Test 11
+>>> cmd.help_write_file()
+==================================================
+write_file command help
+==================================================
+Description: write a PEP8 format .txt file into path chosen
+Syntax: write_file [path]
+Parameter: [path] = full path name of the file starting from the root directory of this program
+Example: write_file test4(myowncode).txt
+<BLANKLINE>
+
+# Test 12 try to read protected file should get permission error message
+>>> cmd.do_load_file("no_permission.txt")
+Error, you do not have permission to access this file!
+
+# Tests for View
+>>> view = View()
+
+# Test 13
+>>> view.print_menu()
+<BLANKLINE>
+Press 1 to load your text file
+Press 2 to write from plantuml text to python code
+Press 3 to run the command line interpreter
+Press 4 to write file to database
+Press 5 to print  class names from database to screen
+Press 6 to exit
+<BLANKLINE>
+
+# Test 14
+>>> view.file_empty_warning()
+File must not be empty
+
+# Test 15
+>>> view.file_not_loaded_warning()
+Must load a file first
+
+# Test 16
+>>> view.file_type_warning()
+File must be a .txt file type
+"""
+
+from View import View
+from Model.ClassFinder import ClassFinder
+from Controller import Controller
+from Controller import CommandLineInterpreter
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    doctest.testmod(verbose=True)
---
+++ View/CommandLineInterpreter.py
@@ -0,0 +1,92 @@
+from cmd import Cmd
+
+
+class CommandLineInterpreter(Cmd):
+    # Created by Adam
+    def __init__(self, controller):
+        Cmd.__init__(self)
+        self.prompt = ">>> "
+        self.my_controller = controller
+        self.banner = "=====" * 10
+
+    # Created by Adam
+    def do_load_file(self, path):
+        self.my_controller.read_file_from_path(path)
+
+    # Created by Adam
+    def do_write_file(self, path):
+        self.my_controller.write_file_to_path(path)
+
+    # Created by Leroi
+    def help_print_file(self):
+        print(
+            self.banner +
+            "\nprint_file command help\n" +
+            self.banner +
+            "\nDescription: Print a PEP8 format text into the interpreter\n"
+            "Syntax: print_file\n"
+            "Example: print_file test4.txt\n"
+        )
+
+    # Created by Leroi
+    def help_write_file(self):
+        print(
+            self.banner +
+            "\nwrite_file command help\n" +
+            self.banner +
+            "\nDescription: write a PEP8 format .txt file into path chosen\n"
+            "Syntax: write_file [path]\n"
+            "Parameter: [path] = full path name of the file starting from the root directory of this program\n"
+            "Example: write_file test4(myowncode).txt\n"
+        )
+
+    # Created by Adam
+    def help_load_file(self):
+        print(
+            self.banner +
+            "\nload_file command help\n" +
+            self.banner +
+            "\nDescription: Load a .txt file into the program\n"
+            "Syntax: load_file [path]\n"
+            "Parameter: [path] = full path name of the file starting from the root directory of this program\n"
+            "Example: load_file test4(myowncode).txt\n"
+        )
+
+    # Created by Adam
+    def help_quit(self):
+        print(
+            self.banner +
+            "\nquit command help\n" +
+            self.banner +
+            "\nDescription: terminate the command line interpreter\n"
+            "Syntax: quit\n"
+            "Parameter: none\n"
+            "Example: quit\n"
+        )
+
+    # Created by Adam
+    def help_greet(self):
+        print(
+            self.banner +
+            "\ngreet command help\n" +
+            self.banner +
+            "\nDescription: A Greeting message\n"
+            "Syntax: greet\n"
+            "Parameter: none\n"
+            "Example: greet\n"
+        )
+
+    # Created by Adam
+    def do_greet(self, line):
+        print(self.banner + "\nWelcome to the command line interpreter."
+                            "\nType help to view available commands\n" + self.banner + "\n")
+
+    # Created by Leroi
+    def do_print_file(self, line):
+        pep8 = self.my_controller.print_file_to_interpreter()
+        print(pep8)
+
+    # Created by Adam
+    def do_quit(self, line):
+        print("Goodbye thank you for using the command line interpreter")
+        return True
