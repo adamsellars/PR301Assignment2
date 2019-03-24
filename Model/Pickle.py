2b0d16a916e62dc6e3b7f54da98ad22f0ae21e3b
LeroiChua <48010810+LeroiChua@users.noreply.github.com>
2019-03-24 20:54:05 +1300

    Add files via upload

--- Model/Pickle.py
+++ Model/Pickle.py
@@ -5,19 +5,31 @@ from tkinter import filedialog
 
 class Pickler:
 
+    # Leroi wrote this
     @staticmethod
     def pickle_file(file_data):
-        with open("data.pickle", "wb") as file:
-            pickle.dump(file_data, file)
+        root = Tk()
+        try:
+            with open("data.pickle", "wb") as file:
+                pickle.dump(file_data, file)
+                root.destroy()
+        except FileNotFoundError:
+            root.destroy()
+            print("Must load a file first")
 
+    # Adam wrote this
     @staticmethod
     def unpickle_file():
         root = Tk()
-        name_of_file = filedialog.askopenfilename(filetypes=(("All files", "*.pickle*"), ("All files", "*.pickle*")))
-        with open(name_of_file, "rb") as file:
-            file_data = pickle.load(file)
-        print(file_data)
-        root.destroy()
+        try:
+            name_of_file = filedialog.askopenfilename(filetypes=(("All files", "*.pickle*"), ("All files", "*.pickle*")))
+            with open(name_of_file, "rb") as file:
+                file_data = pickle.load(file)
+                print(file_data)
+                root.destroy()
+        except FileNotFoundError:
+            root.destroy()
+            print("Must load a file first")
 
 
 
